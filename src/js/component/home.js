77985a5a9cf85a9c9db3187c90fcd60b6786c977
swilcox21 <swilcox21@gmail.com>
2020-10-07 13:11:58 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,54 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
+export function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid">
+			<div id="counter" className="d-flex justify-content-center">
+				<div>
+					<i className="far fa-clock" />
+				</div>
+				<div>{props.number1}</div>
+				<div>{props.number2}</div>
+				<div>{props.number3}</div>
+				<div>{props.number4}</div>
+				<div>{props.number5}</div>
+				<div>{props.number6}</div>
+			</div>
+			<div className="container-fluid d-flex justify-content-center mt-5">
+				<button type="button" className="btn btn-success mx-2">
+					Start
+				</button>
+				<button
+					type="button"
+					onclick="stopCounter"
+					className="btn btn-danger mx-2">
+					Stop
+				</button>
+				<a className="btn btn-info btn-lg mx-2 " href="" role="button">
+					Reset
+				</a>
+			</div>
+			<div className="container-fluid d-flex justify-content-center mt-5">
+				<input type="text" placeholder="enter a number" />
+				<button type="button" className="btn btn-primary">
+					Countdown
+				</button>
+				<button type="button" className="btn btn-warning">
+					Set Alarm
+				</button>
+			</div>
 		</div>
 	);
 }
+Home.propTypes = {
+	number1: PropTypes.string,
+	number2: PropTypes.string,
+	number3: PropTypes.string,
+	number4: PropTypes.string,
+	number5: PropTypes.string,
+	number6: PropTypes.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,31 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+var counter = 0;
+var newCounter;
+function increaseCounter() {
+	counter++;
+	newCounter = "000000" + counter;
+	newCounter = newCounter.slice(-6);
+	console.log(newCounter);
+
+	ReactDOM.render(
+		<Home
+			number1={newCounter.charAt(0)}
+			number2={newCounter.charAt(1)}
+			number3={newCounter.charAt(2)}
+			number4={newCounter.charAt(3)}
+			number5={newCounter.charAt(4)}
+			number6={newCounter.charAt(5)}
+		/>,
+		document.querySelector("#app")
+	);
+	if (newCounter === "000005") {
+		stopCounter();
+	}
+}
+var startCounter = setInterval(increaseCounter, 1000);
+function stopCounter() {
+	clearInterval(startCounter);
+}
